551ded15d58342bce92fb989d951899c84192e7d
CharlesWu-dot <cwudumpling@gmail.com>
2025-04-17 10:45:52 -0400

    commit

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -32,11 +32,24 @@ def generate_launch_description():
 
     # Launch!
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
+    DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use sim time if true'
+    ),
 
-        robot_state_publisher_node,
-        joint_state_publisher_node
-    ])
+    Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_config.toxml()}]
+    ),
+
+    Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen'
+    )
+])
